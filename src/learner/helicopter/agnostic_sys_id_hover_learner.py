68d160b106517c8d0cc84adffe0034623775a463
Anirudh Vemula <vvanirudh@gmail.com>
2022-09-29 13:17:28 -0400

    decreased num iterations

--- src/learner/helicopter/agnostic_sys_id_hover_learner.py
+++ src/learner/helicopter/agnostic_sys_id_hover_learner.py
@@ -47,7 +47,7 @@ def agnostic_sys_id_hover_learner_(
     helicopter_index,
     linearized_model: bool,
     pdl: bool,
-    num_iterations=100,
+    num_iterations=50,
     num_samples_per_iteration=100,
     exploration_distribution_type="desired_trajectory",
     plot=True,
