8726631bc4d64709f7c272614bff4aeb262a66ed
Anirudh Vemula <vvanirudh@gmail.com>
2022-10-03 16:51:53 -0400

    making model class agnostic

--- src/env/helicopter/helicopter_model.py
+++ src/env/helicopter/helicopter_model.py
@@ -64,7 +64,7 @@ class HelicopterModel:
 
 class ParameterizedHelicopterModel:
     def __init__(self, m, Ixx, Iyy, Izz, Tx, Ty, Tz, Fx, Fy, Fz):
-        self.m = m
+        self.m = 6  # m
         self.Ixx = Ixx
         self.Iyy = Iyy
         self.Izz = Izz
